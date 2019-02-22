006a0c64e72115ca430b13ab071a26caa9173c97
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-21 22:11:20 -0800

    Update test_scripts.py

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -1,1 +1,11 @@
 
+from SW import *
+from file_processing import *
+
+def test_align_score():
+    score_mat = read_matrix("score_matrices/BLOSUM50")
+    s1 = "ABCDEFG"
+    s2 = "CDEFG"
+    vary_XX
+    assert max_score==5
+
