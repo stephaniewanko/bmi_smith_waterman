36157d84c4542ffee2fda7189ece3fe141f77d68
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-22 23:37:08 -0800

    Update part_one.py

--- scripts/part_one.py
+++ scripts/part_one.py
@@ -71,6 +71,7 @@ def vary_gap_penalties(pos_fa, neg_fa,max_gap, max_ext,score_mat):
             best_gap=i
             best_ext=j
             print(fpr)
+    #print the final best FPR, gap, ext values
     print(fpr)
     print('Best Gap:')
     print(best_gap)
@@ -85,55 +86,55 @@ def vary_gap_penalties(pos_fa, neg_fa,max_gap, max_ext,score_mat):
 #in terms of false positive rate (at a true positive rate of 0.7)?
 '''
 def pick_best_matrix():
+    '''
+    This script will choose the best matrix based on the FPR at at TPR of 0.7.
+    '''
     matrix_list=['../BLOSUM50', '../BLOSUM62', '../MATIO', '../PAM100', '../PAM250']
-    pos_max_scores=[]
-    neg_max_scores=[]
-    positive_scores={}
-    negative_scores={}
     for i in matrix_list: #iterate over every matrix
-        print(i)
-        positive_score_list = np.zeros(len(positive))
+        print(i) #print the matrix name
+        positive_score_list = np.zeros(len(positive)) #create list to keep list of scores in
         negative_score_list = np.zeros(len(negative))
         matrix=read_scoring_mat(i)
         count=0
         for j in positive:
-            trace,start_pos,max_score,main_bitch=build_matrix(j[0], j[1], 4,3,matrix) #we found out the best gap=5, ext=3
+            trace,start_pos,max_score,main_bitch=build_matrix(j[0], j[1], 4,3,matrix) #we found out the best gap=4, ext=3
             #traceback if you want to return the alignments
-            #=traceback(trace_matrix, start_pos, seq1, seq2, main_matrix)
+            #traceback(trace_matrix, start_pos, seq1, seq2, main_matrix)
             positive_score_list[count] = max_score
             positive_scores[i]=pos_max_scores
             count+=1
         count=0
         for j in negative:
-            trace,start_pos,max_score,main_bitch=build_matrix(j[0], j[1], 4,3,matrix) #we found out the best gap=5, ext=3
+            trace,start_pos,max_score,main_bitch=build_matrix(j[0], j[1], 4,3,matrix) #we found out the best gap=4, ext=3
             negative_score_list[count] = max_score
             count+=1
         sorted_pos_scores=np.sort(positive_score_list)[::-1]
         tpr_cutoff=0.7*len(sorted_pos_scores) #we want to keep the TPR at 0.7. Determine score cutoff at that level.
         score_threshold=sorted_pos_scores[int(tpr_cutoff)] #determine the score threshold
         fpr=len(negative_score_list[negative_score_list>=score_threshold])/len(negative_score_list) #determine the numebr of neg sequences above the score threshold
-        print(fpr)
+        print(fpr) #print the FPR at TPR of 0.7-> used to choose the best matrix
 
 pick_best_matrix()
 '''
 #Question 2b: What are the performance rates of each of the matrices? (create ROC curve)
 '''
 
-def performance_matrix():
+def performance_matrix(): 
+    '''
+    This script will take in all the scoring matrices, calculate their TP, FP, TN, FN and create ROC curves
+    '''
     matrix_list=['BLOSUM50' , 'BLOSUM62', 'MATIO', 'PAM100', 'PAM250']
     for i in matrix_list: #iterate over every matrix
         print(i)
-        #positive_score_list = np.zeros(len(pos_fa))
-        #negative_score_list = np.zeros(len(neg_fa))
-        negative_score_df=pd.DataFrame()
+        negative_score_df=pd.DataFrame() #create df to keep list of scores in to build ROC curve
         positive_score_df=pd.DataFrame()
-        mat_name=('../'+i)
+        mat_name=('../'+i) #import matrix
         print(mat_name)
         matrix=read_scoring_mat(mat_name)
         count=0
-        for j in positive:
+        for j in positive: #for each positive sequence
             trace,start_pos,max_score,main_bitch = build_matrix(j[0], j[1], 4,3,matrix) #we found out the best gap=5, ext=3
-            positive_score_df.loc[count, 'Max_Score'] = max_score
+            positive_score_df.loc[count, 'Max_Score'] = max_score #add to df
             positive_score_df.loc[count, 'Pos_Neg'] = 'Positive'
             count+=1
         count=0
@@ -143,21 +144,18 @@ def performance_matrix():
             negative_score_df.loc[count, 'Pos_Neg'] = 'Negative'
             count+=1
         #combine the df
-        combined = positive_score_df.append(pd.DataFrame(data = negative_score_df), ignore_index=True)
-        combined_sorted=combined.sort_values(['Max_Score'], ascending=False)
+        combined = positive_score_df.append(pd.DataFrame(data = negative_score_df), ignore_index=True) 
+        combined_sorted=combined.sort_values(['Max_Score'], ascending=False) #sort the df to then get the TP, FP, TN, FN for each row
         combined_sorted=combined_sorted.reset_index()
-        for index, row in combined_sorted.iterrows():
+        for index, row in combined_sorted.iterrows(): #for each row, we are going to cut off the DF and determine the number of TP (# of rows with 'positive') above our current cut off, FP (# rows with 'positive') below our current cut off, ect. 
             subset=combined_sorted[0:(index+1)]
-            #len(combined_sorted.loc[combined_sorted['Pos_Neg'] == 'Positive'].index)
-            #print(combined_sorted.loc[,['Pos_Neg'] == 'Positive'])
             combined_sorted.loc[index,'TP']=len(subset.loc[subset['Pos_Neg'] == 'Positive'].index)/(len(combined_sorted.index))
             combined_sorted.loc[index,'FP']=len(subset.loc[subset['Pos_Neg'] == 'Negative'].index)/(len(combined_sorted.index))
             combined_sorted.loc[index,'FN']=(len(positive)-len(subset.loc[subset['Pos_Neg'] == 'Positive'].index))/(len(combined_sorted.index))
             combined_sorted.loc[index,'TN']=(len(negative)-len(subset.loc[subset['Pos_Neg'] == 'Negative'].index))/(len(combined_sorted.index))
-        combined_sorted['FPR']=combined_sorted['FP']/(combined_sorted['FP']+combined_sorted['TN'])
+        combined_sorted['FPR']=combined_sorted['FP']/(combined_sorted['FP']+combined_sorted['TN']) #calculate the FPR/TPR rates
         combined_sorted['TPR']=combined_sorted['TP']/(combined_sorted['TP']+combined_sorted['FN'])
         combined_sorted.to_csv('combined_sorted_'+i+'.csv', sep=',')
-        #print(type(combined_sorted['FP'].values))
         output_name='combined_sorted_'+i
         print(output_name)
         output_name=combined_sorted
@@ -181,6 +179,10 @@ def performance_matrix():
 
 
 def normalization():
+    '''
+    This script will look at normalizing the scores by the shorter of the two sequences comapred to the original scores.
+    This will be done by examining the ROC of the two values 
+    '''
     negative_score_df=pd.DataFrame()
     positive_score_df=pd.DataFrame()
     matrix=read_scoring_mat('../BLOSUM62')
@@ -197,7 +199,7 @@ def normalization():
     for j in negative:
         trace,start_pos,max_score,main_bitch = build_matrix(j[0], j[1], 4,3,matrix) #we found out the best gap=4, ext=3
         test=(j[0], j[1])
-        normalization_factor=(len(min(test, key=len)))
+        normalization_factor=(len(min(test, key=len))) #determine the shorter of the two sequences and return that length
         negative_score_df.loc[count, 'Max_Score_Normalize'] = max_score/normalization_factor
         negative_score_df.loc[count, 'Max_Score'] = max_score
         negative_score_df.loc[count, 'Pos_Neg'] = 'Negative'
@@ -211,7 +213,7 @@ def normalization():
     print('Regular:')
     print(combined_sorted)
     ##
-    for index, row in combined_sorted.iterrows():
+    for index, row in combined_sorted.iterrows(): #for each row, we are going to cut off the DF and determine the number of TP (# of rows with 'positive') above our current cut off, FP (# rows with 'positive') below our current cut off, ect. 
         subset=combined_sorted[0:(index+1)]
         combined_sorted.loc[index,'TP']=len(subset.loc[subset['Pos_Neg'] == 'Positive'].index)/(len(combined_sorted.index))
         combined_sorted.loc[index,'FP']=len(subset.loc[subset['Pos_Neg'] == 'Negative'].index)/(len(combined_sorted.index))
@@ -222,7 +224,7 @@ def normalization():
     combined_sorted.to_csv('combined_sorted.csv', sep=',')
     plt.plot(combined_sorted['FPR'].values,combined_sorted['TPR'].values, label="BLOSUM62")
 
-    ####
+    ####DO IT AGAIN FOR THE NORMALIZED VALUES
     for index, row in combined_sorted_norm.iterrows():
         subset=combined_sorted_norm[0:(index+1)]
         combined_sorted_norm.loc[index,'TP']=len(subset.loc[subset['Pos_Neg'] == 'Positive'].index)/(len(combined_sorted_norm.index))
