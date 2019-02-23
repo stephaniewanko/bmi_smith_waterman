8f60a73b7ecc51c7709f6943ffea630da72110ce
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-22 22:46:00 -0800

    Update test_scripts.py

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -7,8 +7,8 @@ from scripts import file_processing
 #test alignment
 def test_align_score():
     score_mat = file_processing.read_scoring_mat("/home/travis/build/stephaniewanko/bmi_smith_waterman/BLOSUM50")
-    s1 = "ABCDEFG"
-    s2 = "CDEFG"
+    seq1 = "ABCDEFG"
+    seq2 = "CDEFG"
     trace_matrix, max_pos, max_score, middle_matrix=SW.build_matrix(seq1, seq2, gap_cost, gap_ext, scoring_matrix)
     assert max_score==5
 
