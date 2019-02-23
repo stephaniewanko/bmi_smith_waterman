267c96ae74b195f8f5f2bf1ab577092ebb40c609
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-22 22:36:30 -0800

    Update test_scripts.py

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -6,14 +6,14 @@ from scripts import file_processing
 
 #test alignment
 def test_align_score():
-    score_mat = file_processing.read_scoring_mat("../BLOSUM50")
+    score_mat = file_processing.read_scoring_mat("../../BLOSUM50")
     s1 = "ABCDEFG"
     s2 = "CDEFG"
     trace_matrix, max_pos, max_score, middle_matrix=SW.build_matrix(seq1, seq2, gap_cost, gap_ext, scoring_matrix)
     assert max_score==5
 
 def test_optimization():
-    base_mat=file_processing.read_scoring_mat("../BLOSUM62")
+    base_mat=file_processing.read_scoring_mat("../../BLOSUM62")
     new_matrix = file_processing.randomize_matrix(base_mat,i,j)
     assert new_matrix != base_mat
     
