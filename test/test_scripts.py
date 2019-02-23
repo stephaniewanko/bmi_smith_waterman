7150d1db1356798cb093e023efed09d6e456a0f4
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-22 22:57:27 -0800

    Update test_scripts.py

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -10,7 +10,7 @@ def test_align_score():
     seq1 = "ABCDEFG"
     seq2 = "CDEFG"
     trace_matrix, max_pos, max_score, middle_matrix=SW.build_matrix(seq1, seq2, 4, 2, score_mat)
-    assert max_score==5
+    assert max_score==30.0
 
 def test_optimization():
     base_mat=file_processing.read_scoring_mat("/home/travis/build/stephaniewanko/bmi_smith_waterman/BLOSUM62")
